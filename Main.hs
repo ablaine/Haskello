168867e8db58862d8e987836b4121014d13b2322
Andrew Blaine <ablaine@ups.edu>
2009-07-31 19:47:02 -0700

    Added colors to output

--- Main.hs
+++ Main.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import System.IO
 import System.Cmd
 
 import DataTypes
@@ -12,20 +11,18 @@ main :: IO ()
 main = play defaultStart 
 
 defaultStart = GameState othelloBoard X 2 2 4
-header = "<<   Haskello   >>\n"
+header = "<<   Haskello   >>"
 
 play :: GameState -> IO a
 play env = do
 	let b = board env
 	let s = curTurn env
-	let dPts = "X: " ++ show (darkPts env)
-	let lPts = "O: " ++ show (lightPts env)
 	let tPts = "T: " ++ show (totalPts env)
-	let bPrint = showBoard b ++ ["",dPts ++ "\t" ++ lPts ++ "\t" ++ tPts]
 	system "clear"
 	putStrLn header
-	putStrArr bPrint --Print the board
-	putStr $ "\nPlease make your move " ++ stateToStr s ++ ": "
+	printScore (darkPts env) (lightPts env)
+	printBoard b
+	printRequestToMakeMove s
 	move <- getNextMove s b
 	let board' = makeMove (move, s) b
 	let nextTurn = oppState s
--- Utils.hs
+++ Utils.hs
@@ -1,5 +1,6 @@
 module Utils where
 
+import System.Cmd
 import Data.Array
 import Control.Arrow
 
@@ -38,7 +39,10 @@ stateToStr E = "Empty"
 boardToArr :: Board -> [String]
 boardToArr = map handleRow . elems2D
 	where
-		handleRow = concatMap (\i -> if i == E then " ." else if i == O then " o" else " x")
+		handleRow = concatMap (\i -> if i == E then " ." else if i == O then o else x)
+			where
+				x = " " ++ xColor ++ "x" ++ clear
+				o = " " ++ oColor ++ "o" ++ clear
 
 showBoard :: Board -> [String]
 showBoard = prettifyBoard . boardToArr
@@ -47,7 +51,29 @@ showBoard = prettifyBoard . boardToArr
 		prettifyBoard lst = "  0 1 2 3 4 5 6 7" : zipWith (++) (map show [0..7]) lst
 
 putStrArr :: [String] -> IO ()
-putStrArr = mapM_ putStrLn
+putStrArr = mapM_ (\item -> system $ "echo '" ++ item ++ "'")
 
 printBoard :: Board -> IO ()
 printBoard = putStrArr . showBoard
+
+printRequestToMakeMove state = system $ "echo '" ++ "\nPlease make your move " ++ player ++ ": \\c'"
+	where
+		player = if state == X then xColor ++ stateToStr state ++ clear else oColor ++ stateToStr state ++ clear
+
+--error when I try to declare type
+printScore x o = system $ "echo '     " ++ xScore ++ spacing ++ oScore ++ "'"
+	where
+		xScore = xColor ++ show x ++ clear
+		oScore = oColor ++ show o ++ clear
+		spacing = "     " ++ if x >= 10 then "" else " "
+
+{- Edit these to one of the below to choose player colors. -}
+xColor = red
+oColor = green
+
+{- Constants -}
+green = "\\033[0;32m"
+red = "\\033[0;31m"
+yellow = "\\033[0;33m"
+blue = "\\033[0;34m"
+clear = "\\033[0m"
